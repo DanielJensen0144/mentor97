e1d5013c08a39d4f3a4c68431458dc19310e4657
Daniel <jensendaniel0144@gmail.com>
2023-06-22 10:29:06 +0200

    added basic nav with three elements

--- mentor97/mentor97/urls.py
+++ mentor97/mentor97/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import browse
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', browse.views, name="browse")
+    path('home/', views.index, name="browse")
 ]
